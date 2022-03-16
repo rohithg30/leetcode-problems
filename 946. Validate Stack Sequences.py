dabb507df0efcbd78080013fb227f4fc9a2b2afc
ROHITH <92911920+rohithg30@users.noreply.github.com>
2022-03-16 19:33:00 +0530

    Update 946. Validate Stack Sequences.py

--- 946. Validate Stack Sequences.py
+++ 946. Validate Stack Sequences.py
@@ -13,4 +13,21 @@ class Solution:
       
       # T---> O(N)
       # S---> O(N)
-  
+    
+ 
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        
+        i=0
+        while i<len(pushed) and pushed:
+            if pushed[i]==popped[0]:
+                popped.pop(0)
+                pushed.pop(i)
+                i=max(i-1,0)
+            else:
+                i+=1
+        return True if not pushed else False
+    
+            # T--> O(N)
+            # S--> O(1)
+                
